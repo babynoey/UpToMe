83ecdbdb67d22a375d2e707712e58ad5036ff6ed
babynoey <neoy987231@gmail.com>
2021-08-10 22:17:56 +0700

    test that i can use git version control

---
+++ app.js
@@ -0,0 +1,10 @@
+
+        alert("hello Noey")
+        var FirstName = "Natnicha"
+        var LastName = "Thammachote"
+        alert(FirstName)
+        alert(LastName)
+        var input = prompt("กรุณากรอกชื่อเล่นของคุณ")
+        console.log("hello "+input)
+         
+
